0fbdb629860b771c2a54db21de84b3f0a3904c5c
moguimei <AllyCat88201@gmail.com>
2024-01-26 22:11:03 -0500

    Updated GameManager
    added restarts, new games, game over, points if killing citros, etc.

--- Assets/Scripts/Citros.cs
+++ Assets/Scripts/Citros.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class Citros : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int points = 200;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public SirQuack sirQuack;
-    public Citros citros;
+    public Citros[] citros;
     public Transform duckies;
     public int score {get;private set;}
     public int lives {get;private set;}
@@ -13,6 +13,18 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         CreateGame();
+    }
+
+    private void Update()
+    {
+        // if no lives + player presses any key, create new game !
+        if (this.lives <= 0)
+        {
+            if (Input.anyKeyDown)
+            {
+                CreateGame();
+            }
+        }
     }
     private void CreateGame()
     {
@@ -24,18 +36,72 @@ public class GameManager : MonoBehaviour
     private void MakeRound()
     {
 
+        // put the duckies back onto the map in the new round
+        foreach (Transform duckies in this.duckies)
+        {
+            duckies.gameObject.SetActive(true);
+        }
+
+        // calls citros and sir quack
+        DeathRestart();
+    }
+
+    // function that resets the citros and sir Quack's position without changing the pellets
+    private void DeathRestart() 
+    {
+        // put sir quack back onto the map / active
+        this.sirQuack.gameObject.SetActive(true);
+
+        // put the citros back onto the map / set active
+        for (int i = 0; i < this.citros.Length; i++)
+        {
+            this.citros[i].gameObject.SetActive(true);
+        }
+    }
+
+    private void GameOver()
+    {
+        // deactivates sir Quack
+        this.sirQuack.gameObject.SetActive(false);
+
+        // deactivates citros
+        for (int i = 0; i < this.citros.Length; i++)
+        {
+            this.citros[i].gameObject.SetActive(false);
+        }
     }
   
     private void ScorePrepare(int score)
     {
+        // set score
         this.score = score;
     }
     private void LivesPrepare(int lives)
     {
+        // set lives in game
         this.lives = lives;
     }
-    private void Update()
+
+    // when Sir Quack kills Citro with sword, increment score
+    public void CitroDies(Citros citros)
     {
-        
+        ScorePrepare(this.score + citros.points);
+    }
+
+    // when Citro kills Sir Quack, deactivate Sir Quack and take away a life
+    public void SirQuackDies()
+    {
+        this.sirQuack.gameObject.SetActive(false);
+        LivesPrepare(this.lives -= 1);
+
+        // check lives, if lives, reset round AFTER 3 seconds, otherwise end game
+        if (this.lives > 0) 
+        {
+            Invoke(nameof(DeathRestart), 3.0f);
+        }
+        else 
+        {
+            GameOver();
+        }
     }
 }

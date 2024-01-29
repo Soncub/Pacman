7d83da87c66162da61f967d9f3b7e369b5feece0
moguimei <AllyCat88201@gmail.com>
2024-01-29 11:22:29 -0500

    bye bye a*
    commented a* pathing out

--- Assets/Scripts/FindPathAStar.cs
+++ Assets/Scripts/FindPathAStar.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -192,3 +192,4 @@ public class FindPathAStar : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.M)) GetPath();
     }
 }
+*/
